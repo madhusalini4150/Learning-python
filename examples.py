92f227bf75b57383713c81b6ca1927341a5196ab
madhusalini4150 <madhusalinisunkara4150@gmail.com>
2026-02-15 21:46:30 +0530

    Practice Python Examples

--- examples.py
+++ examples.py
@@ -27,3 +27,32 @@ exchange_rate=84.5#set it to a fixed number like 84.50
 amount_usd=float(input("enter amount in usd:"))
 convert_amount=exchange_rate*amount_usd
 print(f"{amount_usd} USD is equal to {convert_amount} in Loacal Currency")
+#--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#Multiplication Table
+#Create a program that prints the multiplication table for a number entered by the user.
+number=int(input("Enter number:"))
+print("The Multiplication Table for given number:")
+for i in range(1,11):
+    print(f"{number} x {i} = {number*i}")
+#------------------------------------------------------------------------------------------------------------------------------------------------
+# Exercise 3.2: The Summation
+# Calculate the sum of all numbers from 1 to 100
+total=0
+for i in range(1,101):
+    total+=i
+print("The sum of all numbers from 1 to 100 is:",total)
+#------------------------------------------------------------------------------------------------------------------------------------------------------
+#Password Retry
+# Create a loop that allows 3 attempts to enter a password
+password="secret"
+attempts=3
+while attempts>0:
+    user_password=input("Enter Password:")
+    if user_password==password:
+        print("Access Granted!!")
+        break
+    else:
+        attempts-=1
+        print("wrong password,attempts left:",attempts)
+if attempts==0:
+    print("Locked Out!!")
