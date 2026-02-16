8bfab306015e97aadaf30c91c10f55fe639642c5
madhusalini4150 <madhusalinisunkara4150@gmail.com>
2026-02-16 21:06:28 +0530

    Printing Patterns

--- day_21.py
+++ day_21.py
@@ -1,20 +1,37 @@
-#printing patterns
+#printing patterns-
+#print a solid square star pattern for a given number n
+n=int(input("enter n:"))
+for i in range(n):
+    for j in range(n):
+        print("*",end=" ")
+    print()
+#--------------------------------------------------------------------------------------
+#printing right angle triangle:   
 rows=5
 for i in range(rows):
     for j in range(i+1):
         print("*",end=' ')
     print()
-#printing above pattern in reverse order
+#----------------------------------------------------------------------------------------
+#printing above pattern in reverse order(reverse right angle triangle)
 print("pattern reversing")
 for i in range(rows,0,-1):
     for j in range(1,i+1):
         print("*",end=" ")
     print()
+     #or
+n=int(input("enter n:"))
+for i in range(n):
+    for j in range(i,n):
+        print('*',end=" ")
+    print()
+#------------------------------------------------------------------------------------------------
 #printing numbers pattern
 for i in range(rows):
     for j in range(i+1):
         print(i,end=" ")
     print(" ")
+#-------------------------------------------------------------------------------------------------
 for i in range(1, rows + 1):  # Outer loop for rows
     for j in range(rows  - i):  # Inner loop for spaces
         print(" ", end=" ")
